e6d234caaafea4bc6322fd0e29e6cb2dcfc68e95
alissakc <44853025+alissakc@users.noreply.github.com>
2021-04-29 17:19:53 -0700

    Updated the main Program with comments

--- src/p2p/Program.java
+++ src/p2p/Program.java
@@ -2,18 +2,14 @@ package p2p;
 
 import java.util.concurrent.ConcurrentSkipListSet;
 
-/**
- * @author bryb
- *
- */
 public class Program {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) throws Exception {
-		IpScanner ipScanner = new IpScanner();
-		ConcurrentSkipListSet networkIps = ipScanner.scan("192.168.0.0", 254);
+		// gets all ip addresses on the network
+		IpScanner ipScanner = new IpScanner();	// create IpScanner object
+		// scans the network to get a list of IP addresses
+		ConcurrentSkipListSet<String> networkIps = ipScanner.scan("192.168.0.0", 254);
+		// prints IP addresses that are reachable out to the console
         System.out.println("Devices connected to the network:");
         networkIps.forEach(ip -> System.out.println(ip));
 	}
