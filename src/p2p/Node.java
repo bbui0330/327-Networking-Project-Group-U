0036007bc50021e48f62da85ce1dcc48141843c9
alissakc <44853025+alissakc@users.noreply.github.com>
2021-05-01 01:33:00 -0700

    Updated the Node class for receiving and sending files

--- src/p2p/Node.java
+++ src/p2p/Node.java
@@ -9,17 +9,13 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.ObjectInputStream;
 import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.net.ConnectException;
 import java.net.InetAddress;
-import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.channels.FileChannel;
-import java.nio.channels.SocketChannel;
-import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.concurrent.ConcurrentSkipListSet;
 
 public class Node {
@@ -29,6 +25,10 @@ public class Node {
 	String path = System.getProperty("user.dir");
 	String folderPath = path + File.separator + "files";
 	File directoryPath = new File(folderPath);
+
+	public final static int FILE_SIZE = 6022386; // file size temporary hard coded
+	// should bigger than the file to be downloaded
+	String FILE =  folderPath + File.separator + "temp1.txt";
 
 	public Node(String type, int port, ConcurrentSkipListSet<String> networkIps) throws IOException {
 		switch (type) {
@@ -40,77 +40,29 @@ public class Node {
 			System.out.println("Connected");
 
 			while(server.isConnected()) {
-				fileHandler.receiveFile("test1.txt");
-				
-//				File [] serverFiles = getListofFiles();
-//				//				System.out.println(java.util.Arrays.toString(getListofFiles()));
-//				Scanner in = new Scanner(server.getInputStream());
-//				InputStream is = server.getInputStream();
-//				PrintWriter pr = new PrintWriter(server.getOutputStream(), true);
-//				String FileName = in.nextLine();
-//				int FileSize = in.nextInt();
-//				FileOutputStream fos = new FileOutputStream(FileName);
-//				BufferedOutputStream bos = new BufferedOutputStream(fos);
-//				byte[] filebyte = new byte[FileSize];
-//
-//				int file = is.read(filebyte, 0, filebyte.length);
-//				bos.write(filebyte, 0, file);
-//
-//				System.out.println("Incoming File: " + FileName);
-//				System.out.println("Size: " + FileSize + "Byte");
-//				if(FileSize == file)System.out.println("File is verified");
-//				else System.out.println("File is corrupted. File Recieved " + file + " Byte");
-//				pr.println("File Recieved Successfully.");
+				// receive file
+				receiveFile(server);
 			}
-			// DataOutputStream out =new DataOutputStream(server.getOutputStream());
-			// out.writeUTF("i am fine, thank you"); DataInputStream in= new
-			// DataInputStream(server.getInputStream()); System.out.println(in.readUTF());
+
 			server.close();
 			break;
 		case "Client":
 			this.port = port;
 			System.out.println("Waiting for connection ...");
-			Socket client;
+			Socket client = null;
 			for (String ip : networkIps) {
 				try {
 					client = new Socket(ip, port);
 					System.out.println("Connected");
-					File [] fileList = getListofFiles();
-					for(File f: fileList) {
-						fileHandler.sendFile(f);
-					}
-//					//	while(client.isConnected()) {
-//					File [] clientFiles = getListofFiles();
-//					//System.out.println(java.util.Arrays.toString(getListofFiles()));
-//					String path = folderPath;
-//					for(File f: clientFiles) {
-//						//String file = path + File.separator + s;
-////						File MyFile = new File(file);
-//						int FileSize = (int) f.length();
-//						OutputStream os = client.getOutputStream();
-//						PrintWriter pr = new PrintWriter(client.getOutputStream(), true);
-//						BufferedInputStream bis = new BufferedInputStream(new FileInputStream(f));
-//						Scanner in = new Scanner(client.getInputStream());
-//
-//						pr.println(f.getName());
-//						pr.println(FileSize);
-//						byte[] filebyte = new byte[FileSize];
-//						bis.read(filebyte, 0, filebyte.length);
-//						os.write(filebyte, 0, filebyte.length);
-//						//				        System.out.println(in.nextLine());
-//						os.flush();
-//					}
-					//	DataInputStream input = new DataInputStream(client.getInputStream());
-					//	System.out.println(input.readUTF()); DataOutputStream output =new
-					//	DataOutputStream(client.getOutputStream());
-					//	output.writeUTF("Hello");
-					//	}
+
+					// send file
+					sendfile(client);
+					
 					client.close();
 				} catch (ConnectException e) {
 					// do nothing
 				}
 			}
-
 			break;
 		}
 
@@ -130,5 +82,41 @@ public class Node {
 	private File[] getListofFiles() {
 		setDirectory();
 		return directoryPath.listFiles();
+	}
+	
+	private void sendfile(Socket socket) throws IOException {
+		// send file
+		for(File myFile: getListofFiles()) {
+			byte [] mybytearrayclient  = new byte [(int)myFile.length()];
+			FileInputStream fis = new FileInputStream(myFile);
+			BufferedInputStream bis = new BufferedInputStream(fis);
+			bis.read(mybytearrayclient,0,mybytearrayclient.length);
+			OutputStream os = socket.getOutputStream();
+			System.out.println("Sending " + FILE + "(" + mybytearrayclient.length + " bytes)");
+			os.write(mybytearrayclient,0,mybytearrayclient.length);
+			os.flush();
+			System.out.println("Done.");
+		}
+	}
+	
+	private void receiveFile(Socket socket) throws IOException {
+		// receive file
+		byte [] mybytearray  = new byte [FILE_SIZE];
+		InputStream is = socket.getInputStream();
+		FileOutputStream fos = new FileOutputStream(FILE);
+		BufferedOutputStream bos = new BufferedOutputStream(fos);
+		int bytesRead = is.read(mybytearray,0,mybytearray.length);
+		int current = bytesRead;
+
+		do {
+			bytesRead =
+					is.read(mybytearray, current, (mybytearray.length-current));
+			if(bytesRead >= 0) current += bytesRead;
+		} while(bytesRead > -1);
+
+		bos.write(mybytearray, 0 , current);
+		bos.flush();
+		System.out.println("File " + FILE
+				+ " downloaded (" + current + " bytes read)");
 	}
 }
